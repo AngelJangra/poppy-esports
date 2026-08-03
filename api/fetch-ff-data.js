1a6fcb11d5a94cd3f3f78841889ec2af12b4e4f6
AngelJangra <jangraangel39@gmail.com>
2026-08-03 10:16:20 +0530

    Update fetch-ff-data.js

--- api/fetch-ff-data.js
+++ api/fetch-ff-data.js
@@ -1,6 +1,5 @@
 // api/fetch-ff-data.js
 export default async function handler(req, res) {
-  // Allow only GET requests
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -11,14 +10,11 @@ export default async function handler(req, res) {
   }
 
   try {
-    // Forward the request to the external HTTP API
     const response = await fetch(`http://187.127.175.208:5000/Bmw?uid=${uid}`);
     if (!response.ok) {
       throw new Error(`API responded with status ${response.status}`);
     }
     const data = await response.json();
-
-    // Return the data to the client
     res.status(200).json(data);
   } catch (error) {
     console.error('Proxy error:', error);
